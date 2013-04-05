69ee779fd08583e190a0b92825a71b632037f789
Rob Hogan <rh389@cam.ac.uk>
2013-04-05 14:55:30 +0000

    Completed SupportNeeds step

--- Process/CourseRegistration/Step/SupportNeeds/SupportNeeds.php
+++ Process/CourseRegistration/Step/SupportNeeds/SupportNeeds.php
@@ -1,26 +1,32 @@
 <?php
 namespace Ice\FormBundle\Process\CourseRegistration\Step\SupportNeeds;
 
-use Ice\JanusClientBundle\Entity\User;
+use Ice\MinervaClientBundle\Entity\StepProgress;
+use Ice\MinervaClientBundle\Exception\NotFoundException;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class SupportNeeds{
     /**
      * @var bool
+     * @Assert\NotNull(groups={"default"}, message="This is a required field")
      */
     private $additionalNeeds;
 
     /**
      * @var string
+     * @Assert\NotBlank(groups={"has_additional_needs"})
      */
     private $additionalNeedsDetail;
 
     /**
      * @var bool
+     * @Assert\NotNull(groups={"default"}, message="This is a required field")
      */
     private $firstFloorAccess;
 
     /**
      * @var bool
+     * @Assert\NotNull(groups={"default"}, message="This is a required field")
      */
     private $shareSupportNeeds;
 
@@ -31,7 +37,7 @@ class SupportNeeds{
      */
     public function setAdditionalNeeds($additionalNeeds)
     {
-        $this->additionalNeeds = $additionalNeeds;
+        $this->additionalNeeds = $additionalNeeds===null?null:($additionalNeeds==true);
         return $this;
     }
 
@@ -67,7 +73,7 @@ class SupportNeeds{
      */
     public function setFirstFloorAccess($firstFloorAccess)
     {
-        $this->firstFloorAccess = $firstFloorAccess;
+        $this->firstFloorAccess = $firstFloorAccess===null?null:($firstFloorAccess==true);
         return $this;
     }
 
@@ -85,7 +91,7 @@ class SupportNeeds{
      */
     public function setShareSupportNeeds($shareSupportNeeds)
     {
-        $this->shareSupportNeeds = $shareSupportNeeds;
+        $this->shareSupportNeeds = $shareSupportNeeds===null?null:($shareSupportNeeds==true);
         return $this;
     }
 
@@ -95,5 +101,28 @@ class SupportNeeds{
     public function getShareSupportNeeds()
     {
         return $this->shareSupportNeeds;
+    }
+
+    /**
+     * @param StepProgress $stepProgress
+     * @return SupportNeeds
+     */
+    public static function fromStepProgress(StepProgress $stepProgress){
+        $instance = new self();
+        return $instance;
+    }
+
+    /**
+     * @param StepProgress $stepProgress
+     * @param $fieldName
+     * @return mixed|null
+     */
+    private function getDeserializedValueByFieldName(StepProgress $stepProgress, $fieldName){
+        try{
+            return $stepProgress->getFieldValueByName($fieldName)->getValue();
+        }
+        catch(NotFoundException $e){
+            return null;
+        }
     }
 }
--- Process/CourseRegistration/Step/SupportNeeds/SupportNeedsType.php
+++ Process/CourseRegistration/Step/SupportNeeds/SupportNeedsType.php
@@ -11,8 +11,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Ice\FormBundle\Process\CourseRegistration\Step\AbstractRegistrationStep;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\MinLength;
-use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Ice\JanusClientBundle\Entity\User;
+
 
 class SupportNeedsType extends AbstractRegistrationStep{
     /**
@@ -22,19 +24,24 @@ class SupportNeedsType extends AbstractRegistrationStep{
     public function buildForm(FormBuilderInterface $builder, array $options){
         $builder
             ->add('additionalNeeds', 'choice', array(
-                    'choices'=>array('Yes', 'No'),
+                    'choices'=>array(
+                        1=>'Yes',
+                        0=>'No'
+                    ),
                     'expanded'=>true,
                     'multiple'=>false,
                     'label'=>'Do you have any additional needs in relation to your learning or access to this course that you would like us to help you with, if we are able to do so?'
                 )
             )
             ->add('additionalNeedsDetail', 'textarea', array(
-                'label'=>'Please specify your additional needs, e.g. induction loop, large print, braille, wheelchair access'
-            ))
+                    'label'=>'Please specify your additional needs, e.g. induction loop, large print, braille, wheelchair access',
+                    'required'=>false,
+                )
+            )
             ->add('firstFloorAccess', 'choice', array(
                     'choices'=>array(
-                        true=>'I AM able to access the first floor without assistance',
-                        false=>'I AM NOT able to access the first floor without assistance'
+                        1=>'I AM able to access the first floor without assistance',
+                        0=>'I AM NOT able to access the first floor without assistance'
                     ),
                     'expanded'=>true,
                     'multiple'=>false,
@@ -42,13 +49,14 @@ class SupportNeedsType extends AbstractRegistrationStep{
                 )
             )
             ->add('shareSupportNeeds', 'choice', array(
-                    'choices'=>array('Yes', 'No'),
+                    'choices'=>array(1=>'Yes', 0=>'No'),
                     'expanded'=>true,
                     'multiple'=>false,
                     'label'=>'Do you consent to this information being passed on to appropriate parties, such as the course administrator or tutor?'
                 )
             )
             ;
+        parent::buildForm($builder, $options);
     }
 
     public function getTemplate(){
@@ -56,7 +64,59 @@ class SupportNeedsType extends AbstractRegistrationStep{
     }
 
     public function prepare(){
-        $this->setEntity(new SupportNeeds());
+        $this->setEntity(SupportNeeds::fromStepProgress($this->getStepProgress()));
         $this->setPrepared();
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function processRequest(Request $request){
+        $this->getForm()->bind($request);
+        /** @var $entity SupportNeeds */
+        $entity = $this->getEntity();
+
+        foreach(array(
+                    2=>'additionalNeeds',
+                    3=>'additionalNeedsDetail',
+                    4=>'firstFloorAccess',
+                    5=>'shareSupportNeeds')
+                as $order=>$fieldName){
+            $getter = 'get'.ucfirst($fieldName);
+            $this->getStepProgress()->setFieldValue(
+                $fieldName,
+                $order,
+                $this->getForm()->get($fieldName)->getConfig()->getOption('label'),
+                $entity->$getter()
+            );
+        }
+
+        if($this->getForm()->isValid()){
+            $this->setComplete();
+        }
+        else{
+            $this->setComplete(false);
+        }
+        $this->setUpdated();
+        $this->save();
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+        $resolver->setDefaults(array(
+            'validation_groups' => function(FormInterface $form) {
+                /** @var $data SupportNeeds */
+                $data = $form->getData();
+                $groups = array('default');
+                if($data->getAdditionalNeeds()){
+                    $groups[] = 'has_additional_needs';
+                }
+                return $groups;
+            }
+        ));
     }
 }
