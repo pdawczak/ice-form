407bc21cd635286a008f53be0088519ebf42ddb7
Rob Hogan <rh389@cam.ac.uk>
2013-05-06 12:34:58 +0000

    Remember selected plan version CS

--- Process/PlaceOrder/Step/ChoosePlans/ChoosePlansType.php
+++ Process/PlaceOrder/Step/ChoosePlans/ChoosePlansType.php
@@ -13,55 +13,57 @@ class ChoosePlansType extends AbstractType
 {
     private $lines = array();
 
-    public function getTemplate(){
+    public function getTemplate()
+    {
         return 'ChoosePlans.html.twig';
     }
 
-    public function getTitle(){
-        return 'Choose payment plan'.(count($this->lines)>0?'s':'');
+    public function getTitle()
+    {
+        return 'Choose payment plan' . (count($this->lines) > 0 ? 's' : '');
     }
 
-    public function isAvailable(){
+    public function isAvailable()
+    {
         return true;
     }
 
-    public function buildForm(FormBuilderInterface $builder, array $options = array()){
-        foreach($this->lines as $line){
+    public function buildForm(FormBuilderInterface $builder, array $options = array())
+    {
+        foreach ($this->lines as $line) {
             /** @var $booking Booking */
             $booking = $line['booking'];
 
             /** @var $plans PaymentPlan[] */
             $plans = $line['plans'];
 
-            $plansPlain = array(
-
-            );
-            foreach($plans as $k=>$v){
+            $plansPlain = array();
+            foreach ($plans as $k => $v) {
                 $plansPlain[$k] = $v['description'];
             }
 
 
-            if(count($this->lines) > 1){
+            if (count($this->lines) > 1) {
                 $plansPlain['later'] = 'Arrange to pay for this booking later';
             }
 
             $data = null;
-            if($existingChoice = $this->getParentProcess()->getProgress()->getPlanChoiceByBookingId($booking->getId())){
+            if ($existingChoice = $this->getParentProcess()->getProgress()->getPlanChoiceByBookingId($booking->getId())) {
                 $data = $existingChoice->getCode();
             }
 
 
-            $builder->add('plan-choice-booking-'.$booking->getId(), 'choice', array(
-                'label'=>'Your choice of payment plan',
-                'expanded'=>false,
-                'multiple'=>false,
-                'data'=>$data,
+            $builder->add('plan-choice-booking-' . $booking->getId(), 'choice', array(
+                'label' => 'Your choice of payment plan',
+                'expanded' => false,
+                'multiple' => false,
+                'data' => $data,
                 'empty_value' => 'Choose a plan...',
-                'choices'=>$plansPlain,
-                'required'=>true,
-                'constraints'=>array(
+                'choices' => $plansPlain,
+                'required' => true,
+                'constraints' => array(
                     new NotBlank(array(
-                        'message'=>'Please choose a payment plan.'
+                        'message' => 'Please choose a payment plan.'
                     ))
                 )
             ));
@@ -70,19 +72,20 @@ class ChoosePlansType extends AbstractType
         parent::buildForm($builder, $options);
     }
 
-    public function processRequest($request){
+    public function processRequest($request)
+    {
         $this->getForm()->bind($request);
         $data = $this->getForm()->getData();
 
-        if($this->getForm()->isValid()){
+        if ($this->getForm()->isValid()) {
             $planChoices = array();
-            foreach($this->lines as $line){
+            foreach ($this->lines as $line) {
                 /** @var $booking Booking */
                 $booking = $line['booking'];
-                $rawChoice = $data['plan-choice-booking-'.$booking->getId()];
-                if($rawChoice!=='later'){
+                $rawChoice = $data['plan-choice-booking-' . $booking->getId()];
+                if ($rawChoice !== 'later') {
                     $matches = array();
-                    if(preg_match("/^([^:]+):(.*)$/", $rawChoice, $matches) === 1){
+                    if (preg_match("/^([^:]+):(.*)$/", $rawChoice, $matches) === 1) {
                         $planChoice = new PlanChoice();
                         $planChoice->setCode($matches[1]);
                         $planChoice->setBookingId($booking->getId());
@@ -105,22 +108,24 @@ class ChoosePlansType extends AbstractType
         return parent::render($vars);
     }
 
-    public function prepare(){
-        if($this->isPrepared())
+    public function prepare()
+    {
+        if ($this->isPrepared())
             return;
 
         $lines = array();
         $paymentPlanService = $this->getParentProcess()->getPaymentPlanService();
 
-        foreach($this->getParentProcess()->getBookingsAvailableToOrder() as $booking){
+        foreach ($this->getParentProcess()->getBookingsAvailableToOrder() as $booking) {
             $course = $this->getParentProcess()->getVeritasClient()->getCourse(
                 $booking->getAcademicInformation()->getCourseId());
             $plans = array();
-            foreach($course->getPaymentPlans() as $plan){
+            foreach ($course->getPaymentPlans() as $plan) {
                 $plans[sprintf("%s:%s", $plan->getCode(), $plan->getVersion())] = array(
-                    'code'=>$plan->getCode(),
-                    'description'=>$paymentPlanService->getPaymentPlan($plan->getCode(), $plan->getVersion())->getShortDescription(),
-                    'receivables'=>$paymentPlanService->getReceivables(
+                    'code' => $plan->getCode(),
+                    'version' => $plan->getVersion(),
+                    'description' => $paymentPlanService->getPaymentPlan($plan->getCode(), $plan->getVersion())->getShortDescription(),
+                    'receivables' => $paymentPlanService->getReceivables(
                         $plan->getCode(),
                         $plan->getVersion(),
                         $course->getStartDate(),
@@ -129,20 +134,22 @@ class ChoosePlansType extends AbstractType
                 );
             }
             $lines[] = array(
-                'booking'=>$booking,
-                'course'=>$course,
-                'plans'=>$plans
+                'booking' => $booking,
+                'course' => $course,
+                'plans' => $plans
             );
         }
         $this->lines = $lines;
         $this->setPrepared();
     }
 
-    public function isComplete(){
+    public function isComplete()
+    {
         return $this->getStepProgress()->isComplete();
     }
 
-    public function getReference(){
+    public function getReference()
+    {
         return 'choosePlans';
     }
 }
