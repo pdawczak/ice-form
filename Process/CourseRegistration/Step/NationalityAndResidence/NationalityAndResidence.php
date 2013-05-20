0267cc4f45fb1ec12342693c411b358b244aaa51
Rob Hogan <rh389@cam.ac.uk>
2013-05-20 18:30:57 +0000

    Add NationalityAndResidence

--- Process/CourseRegistration/Step/NationalityAndResidence/NationalityAndResidence.php
+++ Process/CourseRegistration/Step/NationalityAndResidence/NationalityAndResidence.php
@@ -1,8 +1,288 @@
 <?php
 namespace Ice\FormBundle\Process\CourseRegistration\Step\NationalityAndResidence;
 
-use Ice\JanusClientBundle\Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
+use Ice\MinervaClientBundle\Entity\StepProgress;
+use Ice\MinervaClientBundle\Exception\NotFoundException;
 
-class NationalityAndResidence{
+class NationalityAndResidence
+{
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    private $countryOfResidence;
 
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    private $countryOfBirth;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    private $primaryNationality;
+
+    /**
+     * @var string
+     */
+    private $secondaryNationality;
+
+    /**
+     * @var string
+     */
+    private $ordinarilyResident;
+
+    /**
+     * @var bool
+     */
+    private $eeaOrSwissNational;
+
+    /**
+     * @var bool
+     */
+    private $familyMemberEuNational;
+
+    /**
+     * @var bool
+     */
+    private $settledInUk;
+
+    /**
+     * @var bool
+     */
+    private $grantedRefugeeStatus;
+
+    /**
+     * @var bool
+     */
+    private $requireVisa;
+
+    /**
+     * @var string
+     */
+    private $visaStatus;
+
+    /**
+     * @param string $countryOfBirth
+     * @return NationalityAndResidence
+     */
+    public function setCountryOfBirth($countryOfBirth)
+    {
+        $this->countryOfBirth = $countryOfBirth;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryOfBirth()
+    {
+        return $this->countryOfBirth;
+    }
+
+    /**
+     * @param string $countryOfResidence
+     * @return NationalityAndResidence
+     */
+    public function setCountryOfResidence($countryOfResidence)
+    {
+        $this->countryOfResidence = $countryOfResidence;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryOfResidence()
+    {
+        return $this->countryOfResidence;
+    }
+
+    /**
+     * @param boolean $eeaOrSwissNational
+     * @return NationalityAndResidence
+     */
+    public function setEeaOrSwissNational($eeaOrSwissNational)
+    {
+        $this->eeaOrSwissNational = $eeaOrSwissNational;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getEeaOrSwissNational()
+    {
+        return $this->eeaOrSwissNational;
+    }
+
+    /**
+     * @param boolean $familyMemberEuNational
+     * @return NationalityAndResidence
+     */
+    public function setFamilyMemberEuNational($familyMemberEuNational)
+    {
+        $this->familyMemberEuNational = $familyMemberEuNational;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getFamilyMemberEuNational()
+    {
+        return $this->familyMemberEuNational;
+    }
+
+    /**
+     * @param boolean $grantedRefugeeStatus
+     * @return NationalityAndResidence
+     */
+    public function setGrantedRefugeeStatus($grantedRefugeeStatus)
+    {
+        $this->grantedRefugeeStatus = $grantedRefugeeStatus;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getGrantedRefugeeStatus()
+    {
+        return $this->grantedRefugeeStatus;
+    }
+
+    /**
+     * @param string $ordinarilyResident
+     * @return NationalityAndResidence
+     */
+    public function setOrdinarilyResident($ordinarilyResident)
+    {
+        $this->ordinarilyResident = $ordinarilyResident;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrdinarilyResident()
+    {
+        return $this->ordinarilyResident;
+    }
+
+    /**
+     * @param string $primaryNationality
+     * @return NationalityAndResidence
+     */
+    public function setPrimaryNationality($primaryNationality)
+    {
+        $this->primaryNationality = $primaryNationality;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrimaryNationality()
+    {
+        return $this->primaryNationality;
+    }
+
+    /**
+     * @param boolean $requireVisa
+     * @return NationalityAndResidence
+     */
+    public function setRequireVisa($requireVisa)
+    {
+        $this->requireVisa = $requireVisa;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getRequireVisa()
+    {
+        return $this->requireVisa;
+    }
+
+    /**
+     * @param string $secondaryNationality
+     * @return NationalityAndResidence
+     */
+    public function setSecondaryNationality($secondaryNationality)
+    {
+        $this->secondaryNationality = $secondaryNationality;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecondaryNationality()
+    {
+        return $this->secondaryNationality;
+    }
+
+    /**
+     * @param boolean $settledInUk
+     * @return NationalityAndResidence
+     */
+    public function setSettledInUk($settledInUk)
+    {
+        $this->settledInUk = $settledInUk;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getSettledInUk()
+    {
+        return $this->settledInUk;
+    }
+
+    /**
+     * @param string $visaStatus
+     * @return NationalityAndResidence
+     */
+    public function setVisaStatus($visaStatus)
+    {
+        $this->visaStatus = $visaStatus;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVisaStatus()
+    {
+        return $this->visaStatus;
+    }
+
+
+    /**
+     * @param StepProgress $stepProgress
+     * @return NationalityAndResidence
+     */
+    public static function fromStepProgress(StepProgress $stepProgress)
+    {
+        $instance = new self();
+
+        foreach (get_object_vars($instance) as $fieldName => $currentValue) {
+            $setter = 'set' . ucfirst($fieldName);
+            try {
+                $instance->$setter(
+                    $stepProgress->getFieldValueByName($fieldName)->getValue()
+                );
+            } catch (NotFoundException $e) {
+                //Value not present in the step progress - so leave it at its default
+            }
+        }
+
+        return $instance;
+    }
 }
--- Process/CourseRegistration/Step/NationalityAndResidence/NationalityAndResidenceType.php
+++ Process/CourseRegistration/Step/NationalityAndResidence/NationalityAndResidenceType.php
@@ -2,6 +2,8 @@
 
 namespace Ice\FormBundle\Process\CourseRegistration\Step\NationalityAndResidence;
 
+use Ice\FormBundle\Type\CamsisCountryType;
+use Ice\FormBundle\Type\CountryType;
 use Ice\JanusClientBundle\Exception\ValidationException;
 use JMS\Serializer\Tests\Fixtures\Person;
 use Symfony\Component\Form\FormError;
@@ -14,54 +16,146 @@ use Symfony\Component\Validator\Constraints\MinLength;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Ice\JanusClientBundle\Entity\User;
 
-class NationalityAndResidenceType extends AbstractRegistrationStep{
+class NationalityAndResidenceType extends AbstractRegistrationStep
+{
+    protected $childFormOrder = [
+        1 => 'countryOfResidence',
+        2 => 'countryOfBirth',
+        3 => 'primaryNationality',
+        4 => 'secondaryNationality',
+        5 => 'ordinarilyResident',
+        6 => 'eeaOrSwissNational',
+        7 => 'familyMemberEuNational',
+        8 => 'settledInUk',
+        9 => 'grantedRefugeeStatus',
+        10 => 'requireVisa',
+        11 => 'visaStatus',
+    ];
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options){
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-
+            ->add('countryOfResidence', new CamsisCountryType(), array(
+                    'label' => 'Please select your country of permanent residence (where you normally live). If you live within
+                        the UK, please select England, Scotland, Wales or Northern Ireland as appropriate',
+                    'empty_value'=>'',
+                )
+            )
+            ->add('countryOfBirth', new CountryType(), array(
+                    'label' => 'Country of birth',
+                    'empty_value'=>'',
+                )
+            )
+            ->add('primaryNationality', new CountryType(), array(
+                    'label' => 'Nationality. If you have more than one nationality you should enter the nationality that you consider to be your primary nationality.',
+                    'empty_value'=>'',
+                )
+            )
+            ->add('secondaryNationality', new CountryType(), array(
+                    'label' => 'If you have more than one nationality, you may enter a second nationality.',
+                    'empty_value'=>'',
+                    'required'=>false
+                )
+            )
+            ->add('ordinarilyResident', 'choice', array(
+                    'label' => 'Have you been ordinarily resident in any of the following for at least 3 full years (not for the main purpose of education) prior to the start date of your course? If more than one applies, please select the most recent.',
+                    'empty_value'=>'',
+                    'required'=>false,
+                    'expanded'=>false,
+                    'multiple'=>false,
+                    'choices'=>array(
+                        'EU' => 'EU',
+                        'ISL' => 'Iceland',
+                        'LIE' => 'Lichtenstein',
+                        'NOR' => 'Norway',
+                        'CHE' => 'Switzerland',
+                        'BOT' => 'Overseas territories',
+                        'IMN' => 'Isle of Man',
+                        'NO' => 'NO',
+                    )
+                )
+            )
+            ->add('eeaOrSwissNational', 'choice', array(
+                    'label' => 'Are you an EEA (European Economic Area) or Swiss national living and working in the UK?',
+                    'required'=>false,
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'choices'=>array(
+                        '1' => 'Yes',
+                        '0' => 'No',
+                    )
+                )
+            )
+            ->add('familyMemberEuNational', 'choice', array(
+                    'label' => 'Are you a non-UK/EU national who is a family member of an EU national?',
+                    'required'=>false,
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'choices'=>array(
+                        '1' => 'Yes',
+                        '0' => 'No',
+                    )
+                )
+            )
+            ->add('settledInUk', 'choice', array(
+                    'label' => 'Are you a non-UK/EU citizen settled in the UK?',
+                    'required'=>false,
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'choices'=>array(
+                        '1' => 'Yes',
+                        '0' => 'No',
+                    )
+                )
+            )
+            ->add('grantedRefugeeStatus', 'choice', array(
+                    'label' => 'Have you been granted refugee status in the UK?',
+                    'required'=>false,
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'choices'=>array(
+                        '1' => 'Yes',
+                        '0' => 'No',
+                    )
+                )
+            )
+            ->add('requireVisa', 'choice', array(
+                    'label' => 'Do you require a visa to study in the UK? ',
+                    'required'=>false,
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'choices'=>array(
+                        '1' => 'Yes',
+                        '0' => 'No',
+                    )
+                )
+            )
+            ->add('visaStatus', 'choice', array(
+                    'label' => 'Please enter your current UK visa status, if applicable.',
+                    'required'=>false,
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'choices'=>array( //TODO
+                    )
+                )
+            )
         ;
+
         parent::buildForm($builder, $options);
     }
 
-    /**
-     * @param Request $request
-     */
-    public function processRequest(Request $request){
-        $this->getForm()->bind($request);
-        /** @var $entity AccommodationRequirements */
-        $entity = $this->getEntity();
-
-        foreach(array(
-                )
-                as $order=>$fieldName){
-            $getter = 'get'.ucfirst($fieldName);
-            $this->getStepProgress()->setFieldValue(
-                $fieldName,
-                $order,
-                $this->getForm()->get($fieldName)->getConfig()->getOption('label'),
-                $entity->$getter()
-            );
-        }
-
-        if($this->getForm()->isValid()){
-            $this->setComplete();
-        }
-        else{
-            $this->setComplete(false);
-        }
-        $this->setUpdated();
-        $this->save();
+    public function getHtmlTemplate()
+    {
+        return 'NationalityAndResidence.html.twig';
     }
 
-    public function getHtmlTemplate(){
-        return 'AccommodationRequirements.html.twig';
-    }
-
-    public function prepare(){
-        $this->setEntity(new NationalityAndResidence());
+    public function prepare()
+    {
+        $this->setEntity(NationalityAndResidence::fromStepProgress($this->getStepProgress()));
         $this->setPrepared();
     }
 }
