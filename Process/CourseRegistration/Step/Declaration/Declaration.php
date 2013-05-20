f22f19cc626f88fb94d32a5530c4478e4b32314f
Rob Hogan <rh389@cam.ac.uk>
2013-05-20 14:17:55 +0000

    Implemented declaration step

--- Process/CourseRegistration/Step/Declaration/Declaration.php
+++ Process/CourseRegistration/Step/Declaration/Declaration.php
@@ -1,8 +1,54 @@
 <?php
 namespace Ice\FormBundle\Process\CourseRegistration\Step\Declaration;
 
-use Ice\JanusClientBundle\Entity\User;
+use Symfony\Component\Validator\Constraints as Assert;
+use Ice\MinervaClientBundle\Entity\StepProgress;
+use Ice\MinervaClientBundle\Exception\NotFoundException;
 
-class Declaration{
+class Declaration
+{
+    /**
+     * @var bool
+     */
+    private $qualificationDeclaration;
 
+    /**
+     * @param $qualificationDeclaration
+     * @return Declaration
+     */
+    public function setQualificationDeclaration($qualificationDeclaration)
+    {
+        $this->qualificationDeclaration = $qualificationDeclaration;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getQualificationDeclaration()
+    {
+        return $this->qualificationDeclaration;
+    }
+
+    /**
+     * @param StepProgress $stepProgress
+     * @return DisabilityAndSupportNeeds
+     */
+    public static function fromStepProgress(StepProgress $stepProgress)
+    {
+        $instance = new self();
+
+        foreach (get_object_vars($instance) as $fieldName => $currentValue) {
+            $setter = 'set' . ucfirst($fieldName);
+            try {
+                $instance->$setter(
+                    $stepProgress->getFieldValueByName($fieldName)->getValue()
+                );
+            } catch (NotFoundException $e) {
+                //Value not present in the step progress - so leave it at its default
+            }
+        }
+
+        return $instance;
+    }
 }
--- Process/CourseRegistration/Step/Declaration/DeclarationType.php
+++ Process/CourseRegistration/Step/Declaration/DeclarationType.php
@@ -15,53 +15,31 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Ice\JanusClientBundle\Entity\User;
 
 class DeclarationType extends AbstractRegistrationStep{
+
+    protected $childFormOrder = [
+        1 => 'qualificationDeclaration'
+    ];
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options){
         $builder
+            ->add('qualificationDeclaration', 'checkbox', array(
+                'label'=>'I confirm my understanding'
+            ))
 
         ;
         parent::buildForm($builder, $options);
     }
 
-    /**
-     * @param Request $request
-     */
-    public function processRequest(Request $request){
-        $this->getForm()->bind($request);
-        /** @var $entity AccommodationRequirements */
-        $entity = $this->getEntity();
-
-        foreach(array(
-                )
-                as $order=>$fieldName){
-            $getter = 'get'.ucfirst($fieldName);
-            $this->getStepProgress()->setFieldValue(
-                $fieldName,
-                $order,
-                $this->getForm()->get($fieldName)->getConfig()->getOption('label'),
-                $entity->$getter()
-            );
-        }
-
-        if($this->getForm()->isValid()){
-            $this->setComplete();
-        }
-        else{
-            $this->setComplete(false);
-        }
-        $this->setUpdated();
-        $this->save();
-    }
-
     public function getHtmlTemplate(){
-        return 'AccommodationRequirements.html.twig';
+        return 'Declaration.html.twig';
     }
 
     public function prepare(){
-        $this->setEntity(new Declaration());
+        $this->setEntity(Declaration::fromStepProgress($this->getStepProgress()));
         $this->setPrepared();
     }
 }
