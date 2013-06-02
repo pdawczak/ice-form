fc95af74da049a61a4606e2fc1dc714b6fdfb648
Rob Hogan <rh389@cam.ac.uk>
2013-06-02 11:59:44 +0000

    CS

--- Process/CourseRegistration/Step/AttendeeDetails/AttendeeDetailsType.php
+++ Process/CourseRegistration/Step/AttendeeDetails/AttendeeDetailsType.php
@@ -14,52 +14,55 @@ use Symfony\Component\Validator\Constraints\MinLength;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Ice\JanusClientBundle\Entity\User;
 
-class AttendeeDetailsType extends AbstractRegistrationStep{
+class AttendeeDetailsType extends AbstractRegistrationStep
+{
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options){
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-            ->add('address1', 'text', array('label'=>'Address line 1', 'required'=>true, 'constraints'=>array(new NotBlank())))
-            ->add('address2', 'text', array('label'=>'Address line 2', 'required'=>false))
-            ->add('address3', 'text', array('label'=>'Address line 3', 'required'=>false))
-            ->add('address4', 'text', array('label'=>'Address line 4', 'required'=>false))
-            ->add('city', 'text', array('label'=>'City', 'required'=>true, 'constraints'=>array(new NotBlank())))
-            ->add('postCode', 'text', array('label'=>'Post code', 'required'=>true, 'constraints'=>array(new NotBlank())))
-            ->add('country', 'country', array('label'=>'Country', 'required'=>true, 'constraints'=>array(new NotBlank())))
-            ->add('telephone', 'text', array('label'=>'Telephone', 'required'=>true, 'constraints'=>array(new NotBlank())))
-        ;
+            ->add('address1', 'text', array('label' => 'Address line 1', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('address2', 'text', array('label' => 'Address line 2', 'required' => false))
+            ->add('address3', 'text', array('label' => 'Address line 3', 'required' => false))
+            ->add('address4', 'text', array('label' => 'Address line 4', 'required' => false))
+            ->add('city', 'text', array('label' => 'City', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('postCode', 'text', array('label' => 'Post code', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('country', 'country', array('label' => 'Country', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('telephone', 'text', array('label' => 'Telephone', 'required' => true, 'constraints' => array(new NotBlank())));
         parent::buildForm($builder, $options);
     }
 
     /**
      * @return string
      */
-    public function getTitle(){
+    public function getTitle()
+    {
         return 'Contact';
     }
 
     /**
      * @param Request $request
      */
-    public function processRequest(Request $request = null){
+    public function processRequest(Request $request = null)
+    {
         $this->getForm()->bind($request);
         /** @var $entity AttendeeDetails */
         $entity = $this->getEntity();
 
-        foreach(array(
-                    1=>'address1',
-                    2=>'address2',
-                    3=>'address3',
-                    4=>'address4',
-                    5=>'city',
-                    6=>'postCode',
-                    7=>'country',
-                    8=>'telephone',
-                )
-                as $order=>$fieldName){
-            $getter = 'get'.ucfirst($fieldName);
+        foreach (array(
+                     1 => 'address1',
+                     2 => 'address2',
+                     3 => 'address3',
+                     4 => 'address4',
+                     5 => 'city',
+                     6 => 'postCode',
+                     7 => 'country',
+                     8 => 'telephone',
+                 )
+                 as $order => $fieldName) {
+            $getter = 'get' . ucfirst($fieldName);
             $this->getStepProgress()->setFieldValue(
                 $fieldName,
                 $order,
@@ -68,10 +71,9 @@ class AttendeeDetailsType extends AbstractRegistrationStep{
             );
         }
 
-        if($this->getForm()->isValid()){
+        if ($this->getForm()->isValid()) {
             $this->setComplete();
-        }
-        else{
+        } else {
             $this->setComplete(false);
         }
         $this->setUpdated();
@@ -81,13 +83,15 @@ class AttendeeDetailsType extends AbstractRegistrationStep{
     /**
      * @return mixed|void
      */
-    public function prepare(){
+    public function prepare()
+    {
         $contact = AttendeeDetails::fromStepProgress($this->getStepProgress());
         $this->setEntity($contact);
         $this->setPrepared();
     }
 
-    public function getHtmlTemplate(){
+    public function getHtmlTemplate()
+    {
         return 'AttendeeDetails.html.twig';
     }
 }
