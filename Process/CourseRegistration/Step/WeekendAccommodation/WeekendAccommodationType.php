2d93fb870c08b72e84c327735d2d111faada5881
Rob Hogan <rh389@cam.ac.uk>
2013-06-15 16:38:52 +0000

    Add prices to accommodation options

--- Process/CourseRegistration/Step/WeekendAccommodation/WeekendAccommodationType.php
+++ Process/CourseRegistration/Step/WeekendAccommodation/WeekendAccommodationType.php
@@ -209,9 +209,12 @@ class WeekendAccommodationType extends AbstractRegistrationStep
             }
             foreach ($view->children[$fieldName]->children as $child) {
                 $code = $child->vars['value'];
-                if (!$course->getBookingItemByCode($code)->isInStock()) {
-                    $child->vars['label'] = $options['unavailableMessage'] . ' - ' . $child->vars['label'];
-                    $child->vars['attr']['disabled'] = 'disabled';
+                if ($courseItem = $course->getBookingItemByCode($code)) {
+                    $child->vars['label'].= sprintf(" £%.02f", $courseItem->getPrice()/100);
+                    if (!$course->getBookingItemByCode($code)->isInStock()) {
+                        $child->vars['label'] = $options['unavailableMessage'] . ' - ' . $child->vars['label'];
+                        $child->vars['attr']['disabled'] = 'disabled';
+                    }
                 }
             }
         }
