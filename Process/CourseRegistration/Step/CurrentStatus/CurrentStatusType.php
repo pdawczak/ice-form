2946cdc0dca5799041e092d21726d65d963eaa77
Rob Hogan <rh389@cam.ac.uk>
2014-01-23 17:18:23 +0000

    Add currentStatus step

---
+++ Process/CourseRegistration/Step/CurrentStatus/CurrentStatusType.php
@@ -0,0 +1,165 @@
+<?php
+namespace Ice\FormBundle\Process\CourseRegistration\Step\CurrentStatus;
+
+use Ice\JanusClientBundle\Exception\ValidationException;
+use JMS\Serializer\Tests\Fixtures\Person;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormBuilderInterface;
+use Ice\FormBundle\Process\CourseRegistration\Step\AbstractRegistrationStep;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\MinLength;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Ice\JanusClientBundle\Entity\User;
+
+class CurrentStatusType extends AbstractRegistrationStep
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('currentStatus', 'choice', array(
+                    'choices'=>array(
+                        'Professional'=>'Professional',
+                        'Student'=>'Student',
+                        'Other'=>'Other'
+                    ),
+                    'expanded'=>true,
+                    'multiple'=>false,
+                    'label'=>'What is your current status?'
+                )
+            )
+            ->add('specifyOther', 'text', array('label' => 'Please specify', 'required'=>false))
+            ->add('position', 'text', array('label' => 'Position', 'required'=>false))
+
+            ->add('companyOrInstitution', 'text', array('label' => 'Name of company or institution', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('institutionAddress1', 'text', array('label' => 'Address line 1', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('institutionAddress2', 'text', array('label' => 'Address line 2', 'required' => false))
+            ->add('institutionAddress3', 'text', array('label' => 'Address line 3', 'required' => false))
+            ->add('institutionTown', 'text', array('label' => 'Town', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('institutionCounty', 'text', array('label' => 'County/province/state', 'required' => false))
+            ->add('institutionPostCode', 'text', array('label' => 'Postal code', 'required' => true, 'constraints' => array(new NotBlank())))
+            ->add('institutionCountry', 'country', array('label' => 'Country', 'required' => true, 'constraints' => array(new NotBlank())))
+
+            ->add('courseTitle', 'text', array('label' => 'Title of course', 'required'=>false))
+            ->add('courseStartDate', 'date', array(                    'label' => 'Course start date',
+                'input' => "datetime",
+                'widget' => "choice",
+                'format' => 'd MMM yyyy',
+                'required' => false,
+                'empty_value' => array(
+                    'day' => 'Day',
+                    'month' => 'Month',
+                    'year' => 'Year',
+                )))
+
+        ;
+        parent::buildForm($builder, $options);
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return 'Current status';
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function processRequest(Request $request = null)
+    {
+        $this->getForm()->bind($request);
+        /** @var $entity CurrentStatus */
+        $entity = $this->getEntity();
+
+        foreach (array(
+                     1 => 'currentStatus',
+                     2 => 'specifyOther',
+                     3 => 'position',
+                     4 => 'companyOrInstitution',
+                     5 => 'institutionAddress1',
+                     6 => 'institutionAddress2',
+                     7 => 'institutionAddress3',
+                     8 => 'institutionTown',
+                     9 => 'institutionCounty',
+                     10 => 'institutionPostCode',
+                     11 => 'institutionCountry',
+                     12 => 'courseTitle',
+                     13 => 'courseStartDate'
+                 )
+                 as $order => $fieldName) {
+            $getter = 'get' . ucfirst($fieldName);
+            $value = $entity->$getter();
+
+            $this->getStepProgress()->setFieldValue(
+                $fieldName,
+                $order,
+                $this->getForm()->get($fieldName)->getConfig()->getOption('label'),
+                $value
+            );
+        }
+
+        if ($this->getForm()->isValid()) {
+            $this->setComplete();
+        } else {
+            $this->setComplete(false);
+        }
+        $this->setUpdated();
+        $this->save();
+    }
+
+    /**
+     * @return mixed|void
+     */
+    public function prepare()
+    {
+        if ($this->getStepProgress()->getUpdated()) {
+            $contact = CurrentStatus::fromStepProgress($this->getStepProgress());
+        } else {
+            $contact = new CurrentStatus();
+        }
+        $this->setEntity($contact);
+        $this->setPrepared();
+    }
+
+    public function getHtmlTemplate()
+    {
+        return 'CurrentStatus.html.twig';
+    }
+
+    public function getJavaScriptTemplate(){
+        return 'CurrentStatus.js.twig';
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+        $resolver->setDefaults(array(
+            'validation_groups' => function(FormInterface $form) {
+                /** @var $data CurrentStatus */
+                $data = $form->getData();
+                $groups = array('default');
+                if($data->getCurrentStatus() === 'Professional'){
+                    $groups[] = 'status_professional';
+                }
+                if($data->getCurrentStatus() === 'Student'){
+                    $groups[] = 'status_student';
+                }
+                if($data->getCurrentStatus() === 'Other'){
+                    $groups[] = 'status_other';
+                }
+                return $groups;
+            }
+        ));
+    }
+}
