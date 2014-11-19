1d80fbe51aff745a0e443a296c7825b87b41240b
Rob Hogan <rh389@cam.ac.uk>
2014-11-19 13:59:24 +0000

    Fetch application requirements from Veritas rather than hard-coding them

--- Infrastructure/Veritas/VeritasClientCourseAdapter.php
+++ Infrastructure/Veritas/VeritasClientCourseAdapter.php
@@ -22,20 +22,14 @@ class VeritasClientCourseAdapter
     {
         $courseApplicationRequirements = [];
 
-        /**
-         * TODO: This MUST be changed so that the requirements are built from the course
-         */
-
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('account', '1.0.0', 'Account', 1);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('address', '1.0.0', 'Address', 2);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('education', '1.0.0', 'Education', 3);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('applicationStatement', '1.0.0', 'Personal statement', 4);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('cppaStatement', '1.0.0', 'CPPA Statement', 5);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('englishLanguage', '1.0.0', 'English proficiency', 6);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('supplementaryStatement', '1.0.0', 'Supplementary', 7);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('studentLoan', '1.0.0', 'Student Loan', 8);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('sponsor', '1.0.0', 'Sponsorship', 9);
-        $courseApplicationRequirements[] = new CourseApplicationRequirement('bursary', '1.0.0', 'Bursary', 10);
+        foreach ($vcCourse->getCourseApplicationRequirements() as $vcRequirement)
+        {
+            $courseApplicationRequirements[] = new CourseApplicationRequirement(
+                $vcRequirement->getCode(),
+                $vcRequirement->getVersion(),
+                $vcRequirement->getDescription()
+            );
+        }
 
         $course = (new Course($vcCourse->getId()))
             ->setCourseApplicationRequirements($courseApplicationRequirements)
